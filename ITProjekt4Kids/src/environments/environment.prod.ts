8d56c77666687fb958d94f3eb865f18545e69707
deutho <t_deutsch@gmx.at>
2020-10-16 23:09:25 +0200

    Angular Fire implementiert

--- ITProjekt4Kids/src/environments/environment.prod.ts
+++ ITProjekt4Kids/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  
+  firebase: {
+    apiKey: "AIzaSyAn43XBEtfXPPnO9vrJ-tyMAO-Q3Mvlb1I",
+    authDomain: "kids-8b916.firebaseapp.com",
+    databaseURL: "https://kids-8b916.firebaseio.com",
+    projectId: "kids-8b916",
+    storageBucket: "kids-8b916.appspot.com",
+    messagingSenderId: "184250680836",
+    appId: "1:184250680836:web:fa69884452f2e5240d5751",
+    measurementId: "G-N3XRT4ZVQ9",
+  }
 };
