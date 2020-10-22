6914975ace0b53132b1345453a1ed93b6575b2c0
Thomas <t_deutsch@gmx.at>
2020-10-22 15:50:41 +0200

    created main menu page

--- ITProjekt4Kids/src/app/features/dashboard/main-menu/main-menu.component.ts
+++ ITProjekt4Kids/src/app/features/dashboard/main-menu/main-menu.component.ts
@@ -9,7 +9,22 @@ export class MainMenuComponent implements OnInit {
 
   constructor() { }
 
+  dummyList = ['Wortschatz', 'Verbklammern', 'Satzstellung']
+  creating = false;
   ngOnInit(): void {
+  }
+
+  addElement() {
+    this.creating = true;
+  }
+
+  submit() {
+    if((<HTMLInputElement>document.getElementById('newElement')).value != ''){
+      this.dummyList.push((<HTMLInputElement>document.getElementById('newElement')).value);
+    }
+    
+    (<HTMLInputElement>document.getElementById('newElement')).value = '';
+    this.creating = false;
   }
 
 }
