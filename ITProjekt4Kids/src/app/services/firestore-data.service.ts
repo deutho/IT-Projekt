52a1494490fb435f0134854e05ea0f7731e15782
stefan-hinterhoelzl <hinterhoelzlstefan@gmail.com>
2020-10-24 22:19:35 +0200

    collectionGroup Update

--- ITProjekt4Kids/src/app/services/firestore-data.service.ts
+++ ITProjekt4Kids/src/app/services/firestore-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, fromDocRef } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreCollectionGroup, AngularFirestoreDocument, fromDocRef } from '@angular/fire/firestore';
 import { Observable, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { User } from '../models/users.model';
@@ -11,7 +11,6 @@ import * as firebase from 'firebase';
 @Injectable({ providedIn: 'root' })
 export class FirestoreDataService {
     db = firebase.firestore();
-    dbpath: string = "";
 
     constructor(public _afs: AngularFirestore, public _auth: AuthService) {
     }
@@ -21,48 +20,17 @@ export class FirestoreDataService {
     return null        
     }
 
-    getCurrentUser(): AngularFirestoreDocument<User> {
-        if (this.dbpath == "") this.setDBPath();
-        return this._afs.collection('users').doc(this._auth.getCurrentUser().uid);
+    getCurrentUser(): AngularFirestoreCollectionGroup<User> {
+        console.log(this._auth.getCurrentUser().uid)
+        return this._afs.collectionGroup('users', ref => ref.where('uid', "==", this._auth.getCurrentUser().uid));
+        
     }
 
     addUser(user: User) {
         this.db.collection("users/1lPEcyUVfRVxXsPWbCrOxPjMsrv1/teachers/x4PEJU0ktfOpWBfrvxPgoqPLYgn1/students").doc(user.uid).set(JSON.parse(JSON.stringify(user)));
     }
 
-    private setDBPath() {
-        let uid = this._auth.getCurrentUser().uid;
-        let userRef = this.db.collection('users').doc(uid);
-        var query;
-        let dbfound: boolean = false;
-
-        userRef.get().then((docSnapshot) => {
-            if (docSnapshot.exists) {
-                this.dbpath = "users";
-                dbfound = true;
-            }
-        });
-
-        if (!dbfound) {
-             query = this.db.collectionGroup('teachers').where('id', '==', uid);
-             query.doc(uid).get().then((docSnapshot) => {
-                if (docSnapshot.exists) {
-                    this.dbpath = "teachers";
-                    dbfound = true;
-                }
-             });
-        }
-
-        if (!dbfound) {
-            query = this.db.collectionGroup('students').where('id', '==', uid);
-            query.doc(uid).get().then((docSnapshot) => {
-                if (docSnapshot.exists) {
-                    this.dbpath = "students";
-                    dbfound = true;
-                }
-            });
-        }
-    }
+    
 
 }
 
