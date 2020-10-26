e73fbd3b333e9bf09d41b3261d398d813cb0ad67
Stefan Hinterhölzl <hinterhoelzlstefan@gmail.com>
2020-10-26 18:35:56 +0100

    structure changes

--- ITProjekt4Kids/src/app/features/games/games.module.ts
+++ ITProjekt4Kids/src/app/features/games/games.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { VocabularyGameComponent } from './vocabulary-game/vocabulary-game.component';
+import { VocabularyGameComponent } from '../games/vocabulary-game/vocabulary-game.component';
 
 
 
