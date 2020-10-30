6c250312b4a76e66f8a7a1207f9191516889af0c
Thomas <t_deutsch@gmx.at>
2020-10-30 10:26:16 +0100

    comments

--- ITProjekt4Kids/src/app/features/games/vocabulary-game-edit/vocabulary-game-edit.component.ts
+++ ITProjekt4Kids/src/app/features/games/vocabulary-game-edit/vocabulary-game-edit.component.ts
@@ -28,44 +28,54 @@ export class VocabularyGameEditComponent implements OnInit {
   constructor(private afs: FirestoreDataService, private router: Router, private appService: AppService, private dashboardService: DashboardService) { }
 
   async ngOnInit(): Promise<void> {
+    //get user
     await this.afs.getCurrentUser().valueChanges().pipe(take(1)).toPromise()
       .then(data => this.currentUser = data[0]);
+    //get game data
     await this.afs.getTasksofTeacherbyClass(this.currentUser.uid, '1A').valueChanges().pipe(take(1)).toPromise()
       .then(data => this.Games = data);
+    //init second stack
     let previousGames = [];
     this.previousGames = previousGames;
+
+    //load first game
     this.loadNextGame();
   }
 
   loadNextGame() {    
-    //check for change - beware of undefined if first game
+    this.checkForChanges();
     if (this.Games.length > 0) {
+        //check if first game or already playing
         if(this.currentGame != undefined) {
           this.previousGames.push(this.currentGame);
         }
+        //get next Game
         this.currentGame = this.Games.pop();   
+        //set values of HTML elements
         this.answers = [this.currentGame.rightAnswer, this.currentGame.answer1, this.currentGame.answer2, this.currentGame.answer3];
         this.imageURL = this.currentGame.photoID; //in the meantime set the URL  
+        //makes the HTML elements load new Data
         this.loaded = true;  
     }else {
+      //check if first game or already playing
       if(this.currentGame != undefined) {
         this.previousGames.push(this.currentGame);
       }
+      //fill with placeholder text
       this.answers = ['Richtige Antwort', 'Falsche Antwort 1', 'Falsche Antwort 2', 'Falsche Antwort 3'];
-        this.imageURL = 'https://cdn.pixabay.com/photo/2017/01/18/17/39/cloud-computing-1990405_960_720.png'; //in the meantime set the URL  
-        this.loaded = true;  
-      //this.finishGames() 
+      this.imageURL = 'https://cdn.pixabay.com/photo/2017/01/18/17/39/cloud-computing-1990405_960_720.png';
+      this.loaded = true;  
     }
     
   }
 
   loadPreviousGame() {
     if(this.previousGames.length > 0) {
-      //check for change
+      this.checkForChanges();
       if(this.currentGame != undefined)this.Games.push(this.currentGame);
       this.currentGame = this.previousGames.pop();   
       this.answers = [this.currentGame.rightAnswer, this.currentGame.answer1, this.currentGame.answer2, this.currentGame.answer3];
-      this.imageURL = this.currentGame.photoID; //in the meantime set the URL  
+      this.imageURL = this.currentGame.photoID; 
       this.loaded = true;  
     }
   }
@@ -80,6 +90,7 @@ export class VocabularyGameEditComponent implements OnInit {
   }
 
   checkForChanges(){
+    if(this.currentGame == undefined) return;
     let valueButton1 = document.getElementById('button1').innerText;
     let valueButton2 = document.getElementById('button2').innerText;
     let valueButton3 = document.getElementById('button3').innerText;
@@ -101,8 +112,8 @@ export class VocabularyGameEditComponent implements OnInit {
   }
 
   pictureEdited() {  
+    //toggle to refresh correct image after inputting a new URL
     this.imageURL = (<HTMLInputElement>document.getElementById('URL')).value;
     this.editingPicture = false;       
   }
-
 }
