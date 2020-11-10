4e810b7f1cf18d09451e6d83b5bd12009d11099f
mihoe777 <michael.hoeller1@gmx.net>
2020-11-10 20:33:44 +0100

    Personalformspiel: Grundaufbau implementiert (mit Thomas)

--- ITProjekt4Kids/src/app/features/games/personal-forms-game/personal-forms-game.component.ts
+++ ITProjekt4Kids/src/app/features/games/personal-forms-game/personal-forms-game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {DragDropModule} from '@angular/cdk/drag-drop';
+import {CdkDragDrop, DragDropModule, moveItemInArray} from '@angular/cdk/drag-drop';
+import { PersonalFormsGame } from 'src/app/models/PersonalFormsGame.model';
 
 @Component({
   selector: 'app-personal-forms-game',
@@ -10,7 +11,58 @@ export class PersonalFormsGameComponent implements OnInit {
 
   constructor() { }
 
+  test = "Hallo Welt!"
+  Games: PersonalFormsGame[] = []
+  currentGame: PersonalFormsGame
+  currentGame1 = new PersonalFormsGame("1","gehe","gehst","geht","gehen","geht","gehen","folder")
+  currentGame2 = new PersonalFormsGame("1","sehe","siehst","sieht","sehen","seht","sehen","folder")
+  answers: string[]
+
   ngOnInit(): void {
+    this.Games.push(this.currentGame1)
+    this.Games.push(this.currentGame2)
+    this.shuffleArray(this.Games)
+    this.loadNextGame()
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.answers, event.previousIndex, event.currentIndex);
+  }
+
+  shuffleAnswers() {
+    this.answers = [
+      this.currentGame.ich,
+      this.currentGame.du,
+      this.currentGame.erSieEs,
+      this.currentGame.wir,
+      this.currentGame.ihr,
+      this.currentGame.sie,
+    ];
+    this.shuffleArray(this.answers);
+  }
+
+  shuffleArray(arr) {
+    var currentIndex = arr.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = arr[currentIndex];
+        arr[currentIndex] = arr[randomIndex];
+        arr[randomIndex] = temporaryValue;
+    }
+
+  }
+
+  loadNextGame() {
+
+    if (this.Games.length > 0) {
+        this.currentGame = this.Games.pop();
+        this.shuffleAnswers();        
+        //this.loaded = true;     
+    }
   }
 
 }
---
+++ ITProjekt4Kids/src/app/models/PersonalFormsGame.model.ts
@@ -0,0 +1,13 @@
+export class PersonalFormsGame{
+    
+    public constructor(public uid: string,
+        public ich: string,
+        public du: string,
+        public erSieEs: string,
+        public wir: string,
+        public ihr: string,
+        public sie: string,
+        public folderUID: string) {
+
+    }
+}
