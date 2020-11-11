3815ed45285a821d7f9197a42aef8f3e7c44b081
hinterhoelzl-stefan <hinterhoelzlstefan@gmail.com>
2020-11-11 16:47:32 +0100

    passwort ändern fertig

--- ITProjekt4Kids/src/app/features/dashboard/profile/profile.component.ts
+++ ITProjekt4Kids/src/app/features/dashboard/profile/profile.component.ts
@@ -20,6 +20,7 @@ export class ProfileComponent implements OnInit {
   errorMessage;
   passwordChange = undefined;
   
+  
   constructor(public afs: FirestoreDataService, public auth: AuthService, private fb: FormBuilder) {}
   
   async ngOnInit() {
@@ -32,8 +33,9 @@ export class ProfileComponent implements OnInit {
     else if (this.currentUser.role == 3) this.accountTyp = "Schüler";
 
     this.passwordChangeForm = this.fb.group({
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required)
+      oldPassword: new FormControl('', Validators.required),
+      newPassword: new FormControl('', Validators.required),
+      newPasswordConfirm: new FormControl('', Validators.required),
     });
     this.passwordChange = false;
   }
@@ -42,22 +44,31 @@ export class ProfileComponent implements OnInit {
     this.error = undefined
     this.errorMessage = "";
     this.passwordChange = true;
-
-    let oldPassword :string = this.passwordChange.get('oldPassword').value
-    let newPassword :string = this.passwordChange.get('newPassword').value
-
-
-    await this.auth.changePassword(oldPassword, newPassword).catch((error) => {
-        this.errorMessage = error;
-        this.error = true;
-    });
-
-    if (this.error == undefined) {
-        this.errorMessage = "Passwort wurde geändert"
-        this.error = false;
-    }
   }
 
+  async onSubmit() {
+    let oldPassword :string = this.passwordChangeForm.get('oldPassword').value
+    let newPassword :string = this.passwordChangeForm.get('newPassword').value
+    let newPasswordConfirm :string = this.passwordChangeForm.get('newPasswordConfirm').value
 
-   
+    if (newPassword === newPasswordConfirm) {
+      await this.auth.changePassword(oldPassword, newPassword).catch((error) => {
+          this.errorMessage = error;
+      });
+
+      if (this.errorMessage === "") {
+          this.errorMessage = "Passwort wurde geändert"
+          this.error = false;
+      } else {
+        this.error = true;
+      }
+    } else {
+      this.errorMessage = "Passwort-Bestätigung stimmt nicht überein";
+      this.error = true;
+    }
+
+    setTimeout(() => this.error = undefined, 4000);
+
+  }
+
 }
