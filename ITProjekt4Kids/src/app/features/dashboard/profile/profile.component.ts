677a9ad50decf64c3cd274c63047521e5a98d8dd
hinterhoelzl-stefan <hinterhoelzlstefan@gmail.com>
2020-11-11 15:47:31 +0100

    save

--- ITProjekt4Kids/src/app/features/dashboard/profile/profile.component.ts
+++ ITProjekt4Kids/src/app/features/dashboard/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { take, tap } from 'rxjs/operators';
 import { User } from 'src/app/models/users.model';
 import { AuthService } from 'src/app/services/auth.service';
@@ -11,11 +12,15 @@ import { FirestoreDataService } from 'src/app/services/firestore-data.service';
 })
 export class ProfileComponent implements OnInit {
 
+  passwordChangeForm: FormGroup;
+
   currentUser: User;
   accountTyp: String;
-  error; 
+  error;
+  errorMessage;
+  passwordChange = undefined;
   
-  constructor(public afs: FirestoreDataService, public auth: AuthService) {}
+  constructor(public afs: FirestoreDataService, public auth: AuthService, private fb: FormBuilder) {}
   
   async ngOnInit() {
     await this.afs.getCurrentUser().valueChanges().pipe(take(1)).toPromise().
@@ -25,13 +30,32 @@ export class ProfileComponent implements OnInit {
     if(this.currentUser.role == 1) this.accountTyp = "Adminaccount";
     else if(this.currentUser.role == 2) this.accountTyp = "Lehreraccount";
     else if (this.currentUser.role == 3) this.accountTyp = "Schüler";
+
+    this.passwordChangeForm = this.fb.group({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
+    this.passwordChange = false;
   }
 
-  changePassword(currentPassword, newPassword) {
-  this.auth.changePassword(currentPassword, newPassword).catch((error) => {
-      this.error = error;
-  });
-    
+  async changePassword() {
+    this.error = undefined
+    this.errorMessage = "";
+    this.passwordChange = true;
+
+    let oldPassword :string = this.passwordChange.get('oldPassword').value
+    let newPassword :string = this.passwordChange.get('newPassword').value
+
+
+    await this.auth.changePassword(oldPassword, newPassword).catch((error) => {
+        this.errorMessage = error;
+        this.error = true;
+    });
+
+    if (this.error == undefined) {
+        this.errorMessage = "Passwort wurde geändert"
+        this.error = false;
+    }
   }
 
 
