5e85ee942d4d8e960b3e720cc449294e66fdf47d
hinterhoelzl-stefan <hinterhoelzlstefan@gmail.com>
2020-11-15 13:09:02 +0100

    vocabulary edit texttospeech

--- ITProjekt4Kids/src/app/models/VocabularyGame.model.ts
+++ ITProjekt4Kids/src/app/models/VocabularyGame.model.ts
@@ -1,11 +1,11 @@
 export class VocabularyGame{
     
     public constructor(public uid: string,
-        public answer1: string,
-        public answer2: string,
-        public answer3: string,
-        public rightAnswer: string,
-        public question: string,
+        public answer1: string[],
+        public answer2: string[],
+        public answer3: string[],
+        public rightAnswer: string[],
+        public question: string[],
         public photoID: string,
         public folderUID: string) {
         
