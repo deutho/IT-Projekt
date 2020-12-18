6ef2ef66ed03b37dbd48947e6b07888826ca8d55
paulpuehringer <paul.puehringer@outlook.com>
2020-12-19 00:03:58 +0100

    verb-position: symbole eingefügt

--- ITProjekt4Kids/src/app/features/games/verb-position-game/verb-position-game.component.ts
+++ ITProjekt4Kids/src/app/features/games/verb-position-game/verb-position-game.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/users.model';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import { VerbPositionGame } from 'src/app/models/VerbPositionGame.model';
 import { FirestoreDataService } from 'src/app/services/firestore-data.service';
 import { AppService } from 'src/app/services/app.service';
@@ -18,22 +18,30 @@ export class VerbPositionGameComponent implements OnInit {
 
   ngOnInit(): void {
     this.shuffleArray(this.sentence)
+    this.loadNextGame();
+    this.totalNumberOfRounds = this.Games.length+1; 
   }
 
   Games: VerbPositionGame[] = []
   currentUser: User;
   currentGame: VerbPositionGame
   folderID;
-  answers: string[]
+  // sentence: string[]
   evaluated = false;
-  finished = false;
   answerIsCorrect = false;
   loaded = false; 
+  roundsWon = 0;
+  roundsLost = 0;
+  totalNumberOfRounds = 0;
+  finished = false;
+  speakerMode = false;
 
     sentence = ["Der Affe", "schält", "die Banane"]
+    sentence2 = ["Der Affe", "spielt", "mit der Gitarre"]
+
+    // VerbPositionGame = [this.sentence, this.sentence2]
     correct = this.sentence.slice();
     
-
     drop(event: CdkDragDrop<string[]>) {
       moveItemInArray(this.sentence, event.previousIndex, event.currentIndex); 
       this.capitalizeFirst();
@@ -98,10 +106,13 @@ export class VerbPositionGameComponent implements OnInit {
         if(Result[i] == "falsch") tempAnswerChecker = false;
       }
 
-      if(tempAnswerChecker == true) this.answerIsCorrect = true;
+      if(tempAnswerChecker == true){
+        this.answerIsCorrect = true;
+        if(!this.evaluated) this.roundsWon++;
       //Stimme "Du hast das Toll gemacht!" einfügen
-      else{
-        //Stimme "Oje, das musst du noch üben!"
+      }else{
+        if(!this.evaluated) this.roundsLost++;
+        //Stimme "Oje, probier es noch einmal, du schaffst das!"
       }
       this.evaluated = true;
     }
@@ -149,5 +160,14 @@ export class VerbPositionGameComponent implements OnInit {
 
   finishGame() {
     console.log("fertig");
+    this.finished = true;
   }
+
+  switchMode() {
+    this.loaded = false;
+    this.speakerMode = !this.speakerMode;
+    this.loaded = true;
+    // this.updateColorhelper();
+  }
+
 }
