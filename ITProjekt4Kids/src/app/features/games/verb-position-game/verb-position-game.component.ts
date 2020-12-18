79e27d1838394a4aefc6b95c9a77566dc40b087d
paulpuehringer <paul.puehringer@outlook.com>
2020-12-18 16:52:41 +0100

    verbposition-game: colouring, capitalletter, point

--- ITProjekt4Kids/src/app/features/games/verb-position-game/verb-position-game.component.ts
+++ ITProjekt4Kids/src/app/features/games/verb-position-game/verb-position-game.component.ts
@@ -4,6 +4,8 @@ import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 import { VerbPositionGame } from 'src/app/models/VerbPositionGame.model';
 import { FirestoreDataService } from 'src/app/services/firestore-data.service';
 import { AppService } from 'src/app/services/app.service';
+import { convertCompilerOptionsFromJson } from 'typescript';
+import { last } from 'rxjs/operators';
 
 @Component({
   selector: 'app-verb-position-game',
@@ -15,6 +17,7 @@ export class VerbPositionGameComponent implements OnInit {
   constructor(private afs: FirestoreDataService, private appService: AppService) { }
 
   ngOnInit(): void {
+    this.shuffleArray(this.sentence)
   }
 
   Games: VerbPositionGame[] = []
@@ -27,78 +30,16 @@ export class VerbPositionGameComponent implements OnInit {
   answerIsCorrect = false;
   loaded = false; 
 
-    sentence = ["der Affe", "schält", "die Banane"];
-
-    word1 = ["der Affe"];
-    word2 = ["schält"];
-    word3 = ["die Banane"];
+    sentence = ["Der Affe", "schält", "die Banane"]
+    correct = this.sentence.slice();
     
-    correct = ["der Affe", "schält", "die Banane"];
 
-    showAnswer = false;
-  
     drop(event: CdkDragDrop<string[]>) {
-      this.shuffleArray(this.sentence)
-      moveItemInArray(this.sentence, event.previousIndex, event.currentIndex);
+      moveItemInArray(this.sentence, event.previousIndex, event.currentIndex); 
+      this.capitalizeFirst();
+      this.point();
     }
 
-    evaluateGame(){
-      var Result = []
-      let word1 = (<HTMLElement>document.getElementById('word1'))
-      let word2 = (<HTMLElement>document.getElementById('word2'))
-      let word3 = (<HTMLElement>document.getElementById('word3'))
-      let word4 = (<HTMLElement>document.getElementById('word4'))
-      let word5 = (<HTMLElement>document.getElementById('word5'))
-
-      if (word1.innerText == this.correct[0]) {
-        word1.setAttribute("style","background-color:#52FF82;")
-        Result[0] = "richtig";
-      }else {
-        word1.setAttribute("style", "background-color:#FF7171;")
-        Result[0] = "falsch";
-      }
-
-       if (word2.innerText == this.correct[1]) {
-         word2.setAttribute("style","background-color:#52FF82;")
-         Result[1] = "richtig";
-       }else {
-         word2.setAttribute("style", "background-color:#FF7171;")
-         Result[1] = "falsch";
-       }
-
-      if (word3.innerText == this.correct[2]) {
-         word3.setAttribute("style","background-color:#52FF82;")
-         Result[2] = "richtig";
-       }else {
-         word3.setAttribute("style", "background-color:#FF7171;")
-         Result[2] = "falsch";
-       }
-
-      let tempAnswerChecker = true;
-
-      for( let i in Result) {
-        if(Result[i] == "falsch") tempAnswerChecker = false;
-      }
-
-      if(tempAnswerChecker == true) this.answerIsCorrect = true;
-      //Stimme "Du hast das Toll gemacht!" einfügen
-      this.evaluated = true;
-    }
-
-    shuffleArray(arr) {
-      var currentIndex = arr.length, temporaryValue, randomIndex;
-  
-      while (0 !== currentIndex) {
-  
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex -= 1;
-  
-          temporaryValue = arr[currentIndex];
-          arr[currentIndex] = arr[randomIndex];
-          arr[randomIndex] = temporaryValue;
-      }
-    }
-  
     loadNextGame() {
       this.evaluated = false;
       
@@ -115,10 +56,98 @@ export class VerbPositionGameComponent implements OnInit {
       }
     }
 
+    evaluateGame(){
+      var Result = []
+      var wordId = []
+      //return all ponints of words, which where added before
+      for(var i = 0; i < this.correct.length; i++){
+        var word = this.correct[i]
+
+          if(word.includes(".")){
+            word = word.substr(0, word.length - 1)
+          }
+          this.correct[i] = word
+      }
+
+      //add point to last word
+      var lastWord = this.correct[this.correct.length-1] + "."
+      this.correct[this.correct.length-1] = lastWord
+
+      for(let i = 0; i < this.sentence.length; i++) {
+        let word = (<HTMLElement>document.getElementById('word' + i))
+        wordId[i] = word
+      }
+    
+      //generate a dynamic sentence evaluation
+      for(let i = 0; i < this.sentence.length; i++){
+        
+        if (wordId[i].innerText == this.correct[i]) {
+          wordId[i].setAttribute("style","background-color:#52FF82;")
+          console.log("word " + i + " richtig")
+          Result[i] = "richtig";
+        }else {
+          wordId[i].setAttribute("style", "background-color:#FF7171;")
+          console.log("word " + i + " falsch")
+          Result[i] = "falsch";
+        }
+      }
+
+      let tempAnswerChecker = true;
+
+      for( let i in Result) {
+        if(Result[i] == "falsch") tempAnswerChecker = false;
+      }
+
+      if(tempAnswerChecker == true) this.answerIsCorrect = true;
+      //Stimme "Du hast das Toll gemacht!" einfügen
+      else{
+        //Stimme "Oje, das musst du noch üben!"
+      }
+      this.evaluated = true;
+    }
+
+    shuffleArray(arr) {
+      var currentIndex = arr.length, temporaryValue, randomIndex;
+  
+      while (0 !== currentIndex) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          temporaryValue = arr[currentIndex];
+          arr[currentIndex] = arr[randomIndex];
+          arr[randomIndex] = temporaryValue;
+      }
+    }
+
+    //uppercase first letter of the first box
+    capitalizeFirst() {
+      var capitalizeFirstLetter = this.sentence[0]
+      capitalizeFirstLetter =  capitalizeFirstLetter.charAt(0).toUpperCase() + capitalizeFirstLetter.slice(1)
+      this.sentence[0] = capitalizeFirstLetter
+
+      //change all other first letters of words to lowercase
+      for(var i = 1; i < this.sentence.length; i++){
+        var word = this.sentence[i]
+        word = word.charAt(0).toLowerCase() + word.slice(1)
+        this.sentence[i] = word
+      }
+    }
+
+    //set dynamic point to the end of the sentence
+    point(){
+       for(var i = 0; i < this.sentence.length; i++){
+          var word = this.sentence[i]
+          if(word.includes(".")){
+            word = word.substr(0, word.length - 1)
+          }
+          this.sentence[i] = word
+       }
+      var wordPluspoint = this.sentence[this.sentence.length-1]
+      console.log(wordPluspoint)
+      wordPluspoint = wordPluspoint + "."
+      this.sentence[this.sentence.length-1] = wordPluspoint   
+    }
+
   finishGame() {
     console.log("fertig");
   }
-
-
-
 }
