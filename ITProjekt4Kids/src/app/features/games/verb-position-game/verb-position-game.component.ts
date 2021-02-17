a6d3ecf479fa745d3c0e1de6062c34ce4fa28f2d
deutho <59829810+deutho@users.noreply.github.com>
2021-02-17 20:38:20 +0100

    meeting changes

--- ITProjekt4Kids/src/app/features/games/verb-position-game/verb-position-game.component.ts
+++ ITProjekt4Kids/src/app/features/games/verb-position-game/verb-position-game.component.ts
@@ -150,6 +150,7 @@ export class VerbPositionGameComponent implements OnInit {
   //uppercase first letter of the first box
   capitalizeFirst() {
     var capitalizeFirstLetter = this.sentence[0]
+
     capitalizeFirstLetter =  capitalizeFirstLetter.charAt(0).toUpperCase() + capitalizeFirstLetter.slice(1)
     this.sentence[0] = capitalizeFirstLetter
 
