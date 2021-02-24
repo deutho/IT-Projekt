8f1101f66b65f00074ea0492ff3bb258795b0af2
hinterhoelzl-stefan <hinterhoelzlstefan@gmail.com>
2021-02-24 14:29:45 +0100

    cute cat oooomg

--- ITProjekt4Kids/src/app/features/not-found/not-found.component.ts
+++ ITProjekt4Kids/src/app/features/not-found/not-found.component.ts
@@ -10,20 +10,11 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class NotFoundComponent implements OnInit {
 
-  authstatusSubscription;
-  isAuthenticated;
-
   constructor(private router: Router, private app: AppService, private auth: AuthService) { }
 
   ngOnInit(): void {
 
-    this.app.myHeader("Oje");
-    this.authstatusSubscription = this.auth.currentAuthStatus.subscribe(authstatus => this.isAuthenticated = authstatus)
-  }
-
-
-  goBack() {
-    this.router.navigate(['app/ ']);
+    this.app.myHeader("Diese Seite existiert nicht");
   }
 
 }
